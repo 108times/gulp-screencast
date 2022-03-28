202a4e7ead61dc263765ba9607b027245fc2d55f
Amir Kadenov <amirkadenov@gmail.com>
2022-03-28 22:41:39 +0600

    Initial commit. Finished part 4

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const gulp = require("gulp");
+const stylus = require("gulp-stylus");
+const concat = require("gulp-concat");
+const sourcemaps = require("gulp-sourcemaps");
+const debug = require("gulp-debug");
+const gulpIf = require("gulp-if");
+const del = require("del");
+
+const isDevelopment =
+  !process.env.NODE_ENV || process.env.NODE_ENV == "development";
+
+gulp.task("styles", function () {
+  return gulp
+    .src("frontend/**/*.styl")
+    .pipe(gulpIf(isDevelopment, sourcemaps.init()))
+    .pipe(stylus())
+    .pipe(concat("all.css"))
+    .pipe(gulpIf(isDevelopment, sourcemaps.write()))
+    .pipe(gulp.dest("public"));
+});
+
+gulp.task("clean", function () {
+  return del("public");
+});
+gulp.task("assets", function () {
+  return gulp.src("frontend/assets/**").pipe(gulp.dest("public"));
+});
+
+gulp.task("build", gulp.series("clean", gulp.parallel(["styles", "assets"])));
