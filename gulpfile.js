41c9389437a3e77adc67625686ce20c55c03e4a3
Amir Kadenov <amirkadenov@gmail.com>
2022-03-31 16:37:20 +0600

    Finished part 8

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,85 @@
-"use strict";
+'use strict';
 
-const gulp = require("gulp");
-const stylus = require("gulp-stylus");
-const concat = require("gulp-concat");
-const sourcemaps = require("gulp-sourcemaps");
-const debug = require("gulp-debug");
-const gulpIf = require("gulp-if");
-const del = require("del");
+const gulp = require('gulp');
+const stylus = require('gulp-stylus');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+const debug = require('gulp-debug');
+const gulpIf = require('gulp-if');
+const del = require('del');
+const newer = require('gulp-newer');
+const browserSync = require('browser-sync').create();
+const notify = require('gulp-notify');
+const plumber = require('gulp-plumber');
+const multipipe = require('multipipe');
 
-const isDevelopment =
-  !process.env.NODE_ENV || process.env.NODE_ENV == "development";
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
-gulp.task("styles", function () {
+gulp.task('styles', function () {
+  return multipipe(
+    gulp.src('frontend/**/*.styl'),
+
+    gulpIf(isDevelopment, sourcemaps.init()),
+    stylus(),
+    concat('all.css'),
+    gulpIf(isDevelopment, sourcemaps.write()),
+    gulp.dest('public'),
+  ).on(
+    'error',
+
+    notify.onError(function (err) {
+      return {
+        title: 'Styles',
+        message: err.message,
+      };
+    }),
+  );
+
+  // gulp
+  //   .src('frontend/**/*.styl')
+  //   .pipe(
+  //     plumber({
+  //       errorHandler: notify.onError(function (err) {
+  //         return {
+  //           title: 'Styles',
+  //           message: err.message,
+  //         };
+  //       }),
+  //     }),
+  //   )
+  //   .pipe(gulpIf(isDevelopment, sourcemaps.init()))
+  //   .pipe(stylus())
+
+  //   .pipe(concat('all.css'))
+  //   .pipe(gulpIf(isDevelopment, sourcemaps.write()))
+  //   .pipe(gulp.dest('public'));
+});
+
+gulp.task('clean', function () {
+  return del('public');
+});
+
+gulp.task('assets', function () {
   return gulp
-    .src("frontend/**/*.styl")
-    .pipe(gulpIf(isDevelopment, sourcemaps.init()))
-    .pipe(stylus())
-    .pipe(concat("all.css"))
-    .pipe(gulpIf(isDevelopment, sourcemaps.write()))
-    .pipe(gulp.dest("public"));
+    .src('frontend/assets/**')
+    .pipe(newer('public'))
+    .pipe(debug({ title: 'assets' }))
+    .pipe(gulp.dest('public', gulp.lastRun('assets')));
 });
 
-gulp.task("clean", function () {
-  return del("public");
-});
-gulp.task("assets", function () {
-  return gulp.src("frontend/assets/**").pipe(gulp.dest("public"));
+gulp.task('watch', function () {
+  gulp.watch('frontend/styles/**/*.*', gulp.series('styles'));
+  gulp.watch('frontend/assets/**/*.*', gulp.series('assets'));
 });
 
-gulp.task("build", gulp.series("clean", gulp.parallel(["styles", "assets"])));
+gulp.task('build', gulp.series('clean', gulp.parallel(['styles', 'assets'])));
+
+gulp.task('serve', function () {
+  browserSync.init({
+    server: 'public',
+  });
+
+  browserSync.watch('public/**/*.*').on('change', browserSync.reload);
+});
+
+gulp.task('dev', gulp.series('build', gulp.parallel('watch', 'serve')));
--- gulpfile_old.js
+++ gulpfile_old.js
@@ -1,24 +1,34 @@
-'use strict';
+"use strict";
 
-const gulp = require('gulp');
+const gulp = require("gulp");
+const concat = require("gulp-concat");
+const debug = require("gulp-debug");
+const autoprefixer = require("gulp-autoprefixer");
+const remember = require("gulp-remember");
+const stylus = require("gulp-stylus");
+const cached = require("gulp-cached");
+const path = require("path");
 
-gulp.task('hello', function(callback) {
-  console.log('hello')
-  callback()
-})
+gulp.task("styles", function () {
+  return gulp
+    .src(
+      "frontend/styles/**/*.styl"
+    ) /*{since: gulp.lastRun("styles"),} // mtime }*/
+    .pipe(cached("styles")) // content
+    .pipe(remember("styles"))
+    .pipe(stylus())
+    .pipe(autoprefixer())
+    .pipe(concat("all.css"))
+    .pipe(gulp.dest("public"));
+});
 
-gulp.task('example:promise', function() {
-  return new Promise(((resolve, reject) => {
-    resolve('result')
-  } ))
-})
+gulp.task("watch", function () {
+  gulp
+    .watch("frontend/styles/**/*.styl", gulp.series("styles"))
+    .on("unlink", function (filepath) {
+      remember.forget("styles", path.resolve(filepath));
+      delete cached.caches.styles[path.resolve(filepath)];
+    });
+});
 
-gulp.task('example:stream', function() {
-  return require('fs').createReadStream(__filename)
-})
-
-gulp.task('example:process', function() {
-  return require('child_process').spawn(['ls',['node_modules'], {stdio: 'inherit'}])
-})
-
-gulp.task('example', gulp.series('hello','example:promise','example:stream','example:process'))
+gulp.task("dev", gulp.series("styles", "watch"));
